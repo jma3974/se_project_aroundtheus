b70a8f380e4c8cc83739c958fb4ae2adbd83cfb0
jma3974 <jma3974@gmail.com>
2023-01-04 08:43:21 -0800

    add arrays and objects

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,38 @@
-let initialCards =[destinationCard_01, destinationCard_02, destinationCard_03, destinationCard_04, destinationCard_05, destinationCard_06]
+let destinationCard_01 = {
+  name: "Yosemite Valley",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+};
 
+let destinationCard_02 = {
+  name: "Lake Louise",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+};
 
-let destinationCard_01={ 
-    name: "Yosemite Valley"
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
-}
+let destinationCard_03 = {
+  name: "Bald Mountains",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+};
 
-let destinationCard_02={
-    name: "Lake Louise"
-    link: "Lake Louise"
-}
+let destinationCard_04 = {
+  name: "Latemar",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+};
 
-let destinationCard_03={
-    name: "Bald Mountains"
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
-}
+let destinationCard_05 = {
+  name: "Vanoise National Park",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+};
 
-let destinationCard_04={
-    name: "Latemar"
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
-}
+let destinationCard_06 = {
+  name: "Lago di Braies",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+};
 
-let destinationCard_05={
-    name: "Vanoise National Park"
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
-}
-
-let destinationCard_06={
-    name: "Lago di Braies"
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
-}
+const initialCards = [
+  destinationCard_01,
+  destinationCard_02,
+  destinationCard_03,
+  destinationCard_04,
+  destinationCard_05,
+  destinationCard_06,
+];
